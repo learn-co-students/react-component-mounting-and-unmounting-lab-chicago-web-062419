a92c30a47c62339e325117757b968a5b61640185
github username <github email address>
2019-08-29 12:51:09 -0500

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -11,6 +11,13 @@ class Pancake extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
+  componentDidMount(){
+    this.startInterval();
+  }
+
+  componentWillUnmount(){
+    this.cleanUpInterval();
+  }
 
   // TODO: create a componentWillUnmount() which will clear the interval
 
